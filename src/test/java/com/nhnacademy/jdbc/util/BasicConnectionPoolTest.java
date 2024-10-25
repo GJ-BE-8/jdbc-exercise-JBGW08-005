3bf64dbc9d1253cda04625ad8d1782e003de346e
kimkihyun <kkhkh4531@gmail.com>
2024-10-25 11:01:53 +0900

    connection pool commit

--- src/test/java/com/nhnacademy/jdbc/util/BasicConnectionPoolTest.java
+++ src/test/java/com/nhnacademy/jdbc/util/BasicConnectionPoolTest.java
@@ -21,7 +21,7 @@ class BasicConnectionPoolTest {
     @BeforeAll
     static void setUp() {
         //todo#0 - jdbcUrl, username, password를 설정하세요
-        basicConnectionPool = new BasicConnectionPool(com.mysql.cj.jdbc.Driver.class.getName(),"","","",5);
+        basicConnectionPool = new BasicConnectionPool(com.mysql.cj.jdbc.Driver.class.getName(),"jdbc:mysql://133.186.241.167:3306/nhn_academy_5","nhn_academy_5","1Nv@Fbn6IU94!8EJ",5);
     }
 
     @AfterAll
